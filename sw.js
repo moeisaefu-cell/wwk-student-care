62dc94e2f00f76c3980288f9e7bcb8dd0fda68dc
moeisaefu-cell <moeisaefu@gmail.com>
2026-07-05 03:16:57 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,8 @@
+// Service Worker เบื้องต้นสำหรับรองรับ PWA
+self.addEventListener('install', (e) => {
+  // ติดตั้งเสร็จสมบูรณ์
+});
+
+self.addEventListener('fetch', (e) => {
+  // จัดการดึงข้อมูลแคชเพื่อความรวดเร็วเมื่อใช้งานแอป
+});
